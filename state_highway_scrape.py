4fe6ae877fa661fe2e9958c3f85bac0c74990ba4
Kelly <kellykenoyerwriter@gmail.com>
2019-10-30 10:59:08 -0500

    got the html

--- state_highway_scrape.py
+++ state_highway_scrape.py
@@ -1,5 +1,10 @@
-import requests
+
 from bs4 import BeautifulSoup
+import requests, mechanize
+import csv
+
+csvfile = open('highway.csv', 'a')
+hwy_writer =csv.writer(csvfile)
 
 url = "https://www.mshp.dps.missouri.gov/HP68/SearchAction"
 
@@ -8,3 +13,18 @@ dictionary = {'searchDate' : '10/31/2017'}
 r = requests.post(url, data=dictionary)
 
 print(r.text)
+
+html = r.text
+
+soup = BeautifulSoup(html, "html.parser")
+
+print(soup)
+
+
+
+
+
+
+
+
+
